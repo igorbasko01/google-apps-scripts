7a6d2836c53678f5cc7a4056bc3e400f6c51b15f
igorbasko01 <igorbasko@gmail.com>
2023-08-04 14:09:47 +0300

    add simple test

--- tasks-scheduler/Code.ts
+++ tasks-scheduler/Code.ts
@@ -8,7 +8,11 @@ function main() {
   listScriptProperties();
 }
 
-function storeScriptProperties(key, value) {
+function storeScriptProperties(key: string, value: string) {
   var scriptProperties = PropertiesService.getScriptProperties();
   scriptProperties.setProperty(key, value);
+}
+
+export function add(a: number, b: number): number {
+  return a + b;
 }
